87575123319de3250419a4c79d59f39bab313172
SaltyChocolat <rexrares009@gmail.com>
2024-10-21 17:53:08 +0300

    Added NewGridSize n Reset Functions +UI

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-const container = document.querySelector("#container")
 
+const container = document.querySelector("#container");
+const newGridButton = document.querySelector("button");
+const resetButton = document.querySelector("#reset")
+const input = document.querySelector("input"); 
+let x;
 
-for(let i=0;i<16;i++){
-    row = document.createElement("div")
-    row.classList.add("row")
-    for(let j=0;j<16;j++){
-        box = document.createElement("div")
-        box.classList.add("box")
-        box.addEventListener("mouseover",function(){
-            box.style.backgroundcolor = "grey";
-        })
-        row.appendChild(box)
+function createGrid(x){
+    if(x>100){
+        alert("Number too big");
+        return
     }
-    container.appendChild(row)
+    for(let i=0;i<x;i++){
+        row = document.createElement("div")
+        row.classList.add("row")
+        for(let j=0;j<x;j++){
+            box = document.createElement("div")
+            box.classList.add("box")
+            row.appendChild(box)
+        }
+        container.appendChild(row)
+    }
+    return x;
 }
+x=createGrid(100)
+
+container.addEventListener("mouseover",function(event){
+    target = event.target;
+    target.classList.toggle("colored");
+})
+
+newGridButton.addEventListener("click",function(){
+    value = Number(input.value)
+    if(Number.isInteger(value)){
+        while(container.firstChild){
+            container.firstChild.remove()
+        }
+        createGrid(value)
+    }
+    else{
+        input.focus()
+        alert("Type Number")
+    }
+})
+
+resetButton.addEventListener("click",function(){
+    while(container.firstChild){
+        container.firstChild.remove()
+    }
+    createGrid(x)
+})
